b4edf2dd227655d253e808cd99300bdaa6b1a254
Raj Kunwar Prabhat <rajkunwarprabhat@gmail.com>
2025-04-30 14:43:20 +0530

    Added Casting

---
+++ Casting/Program.cs
@@ -0,0 +1,34 @@
+﻿// int first = 2;
+// string second = "4";
+// string result = first + second;
+// Console.WriteLine(result);
+// int myInt = 3;
+// Console.WriteLine($"int: {myInt}");
+
+// decimal myDecimal = myInt;
+// Console.WriteLine($"decimal: {myDecimal}");
+// decimal myDecimal = 3.14m;
+// Console.WriteLine($"decimal: {myDecimal}");
+
+// int myInt = (int)myDecimal;
+// Console.WriteLine($"int: {myInt}");
+// decimal myDecimal = 1.23456789m;
+// float myFloat = (float)myDecimal;
+
+// Console.WriteLine($"Decimal: {myDecimal}");
+// Console.WriteLine($"Float  : {myFloat}");
+// int first = 7;
+// int second = 5;
+// string message  = first.ToString() + second.ToString();
+// Console.WriteLine(message);
+// string first = "7";
+// string second = "8";
+// int sum = int.Parse(first) + int.Parse(second);
+// Console.WriteLine(sum);
+
+//Convert a string to a int using the Convert class
+// string value1 = "10";
+// string value2 = "20";
+
+// int result = Convert.ToInt32(value1) + Convert.ToInt32(value2);
+// Console.WriteLine(result);
