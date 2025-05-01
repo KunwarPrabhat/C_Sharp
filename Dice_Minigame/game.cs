5455083f621279ead427e051bd6560969976be88
Raj Kunwar Prabhat <rajkunwarprabhat@gmail.com>
2025-05-01 13:23:11 +0530

    Added game.cs

--- Dice_Minigame/game.cs
+++ Dice_Minigame/game.cs
@@ -1,25 +1,48 @@
 Random random = new Random();
 
 Console.WriteLine("Would you like to play? (Y/N)");
-if (ShouldPlay()) 
+if (ShouldPlay())
 {
     PlayGame();
 }
+bool ShouldPlay()
+{
+    string userInput = Console.ReadLine();
 
-void PlayGame() 
+    if (userInput == "Y") { return true; }
+    else if (userInput == "N") { return false; }
+    else
+    {
+        Console.WriteLine("Enter the valid Option!.");
+        return ShouldPlay();
+    }
+}
+
+void PlayGame()
 {
     var play = true;
 
-    while (play) 
+    while (play)
     {
-        var target;
-        var roll;
+        var target = random.Next(1, 6);
+        var roll = random.Next(1, 7);
 
         Console.WriteLine($"Roll a number greater than {target} to win!");
         Console.WriteLine($"You rolled a {roll}");
-        Console.WriteLine(WinOrLose());
+        Console.WriteLine(WinOrLose(target, roll));
         Console.WriteLine("\nPlay again? (Y/N)");
 
         play = ShouldPlay();
+    }
+}
+string WinOrLose(int val1, int val2)
+{
+    if (val2 > val1)
+    {
+        return "You win!";
+    }
+    else
+    {
+        return "You lose!";
     }
 }
